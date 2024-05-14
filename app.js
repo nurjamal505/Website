238805c289ea139b8b3b546a95f5294c86d419cb
Nurjamal Osmonalieva <nurjamal7098@gmail.com>
2024-05-14 23:08:14 +0600

    tenth commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const menuItems = document.querySelectorAll(".menuItem");
 
 menuItems.forEach((item, index)=>{
     item.addEventListener("click", ()=>{
-        console.log("you clicked!");
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
     });
 });
